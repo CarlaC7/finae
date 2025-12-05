0c0b651aac44927f0e2b46cf205c94787c782839
CarlaC7 <calleapazacarla@gmail.com>
2025-12-05 18:22:11 -0400

    Add files via upload

---
+++ vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,40 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-7DDIEL4Y.js";
+import "./chunk-BVVNRTKV.js";
+import "./chunk-PAUXKQHA.js";
+import "./chunk-YP27K3CD.js";
+import "./chunk-L3JOR4S4.js";
+import "./chunk-AWTC2KQO.js";
+import "./chunk-OU2B74ND.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
